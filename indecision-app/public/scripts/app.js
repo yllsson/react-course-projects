7de2a5a66d5d3ad23e22c514e6a70a7903d8b303
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-03-25 20:48:11 +0000

    Initial commit

---
+++ indecision-app/public/scripts/app.js
@@ -0,0 +1,14 @@
+console.log('app.js is running!');
+
+// JSX - JavaScript XML
+// var template = <p>This is JSX from app.js</p>;
+var template = /*#__PURE__*/ React.createElement(
+  'h1',
+  {
+    id: 'someid'
+  },
+  'Something new!'
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
