529c9a25366e25d60919929c215303b6af028ff2
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-03-31 21:01:49 +0100

    Create my-cheat-sheet.js

---
+++ indecision-app/src/playground/my-cheat-sheet.js
@@ -0,0 +1,8 @@
+/*
+Frequently used command line prompts: 
+
+Start up Babel watching: babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+
+Start up live server (need to be in indecision-app folder): live-server public
+
+*/
