82f1efaf682c7b01069685e84aa2dd49004bc5ba
Ylva-Thorsen <ylva.e.thorsen@gmail.com>
2021-04-05 12:15:15 +0100

    Adding counter and buttons

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -8,18 +8,26 @@ var app = {
   options: ['One', 'Two']
 };
 var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, app.title), app.subtitle && /*#__PURE__*/React.createElement("p", null, app.subtitle), /*#__PURE__*/React.createElement("p", null, app.options.length > 0 ? 'These are your options' : 'You have no options'), /*#__PURE__*/React.createElement("ol", null, /*#__PURE__*/React.createElement("li", null, "Item one"), /*#__PURE__*/React.createElement("li", null, "Item two")));
-var user = {
-  name: 'Ylva Turner',
-  age: 30,
-  location: 'Edinburgh'
+var count = 0;
+
+var addOne = function addOne() {
+  console.log('addOne');
 };
 
-function getLocation(location) {
-  if (location) {
-    return /*#__PURE__*/React.createElement("p", null, "Location: ", location);
-  }
-}
+var minusOne = function minusOne() {
+  console.log('minusOne');
+};
 
-var templateTwo = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, user.name ? user.name : 'Anonymous'), user.age && user.age >= 18 && /*#__PURE__*/React.createElement("p", null, "Age: ", user.age), getLocation(user.location));
+var reset = function reset() {
+  console.log('reset');
+};
+
+var templateTwo = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Count: ", count), /*#__PURE__*/React.createElement("button", {
+  onClick: addOne
+}, "+1"), /*#__PURE__*/React.createElement("button", {
+  onClick: minusOne
+}, "-1"), /*#__PURE__*/React.createElement("button", {
+  onClick: reset
+}, "Reset"));
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -5,7 +5,7 @@ console.log('app.js is running!');
 const app = {
   title: 'Welcome to the Indecision App!',
   subtitle: 'We will help you make up your mind!',
-  options: ['One', 'Two']
+  options: ['One', 'Two'],
 };
 
 const template = (
@@ -24,26 +24,25 @@ const template = (
   </div>
 );
 
-const user = {
-  name: 'Ylva Turner',
-  age: 30,
-  location: 'Edinburgh'
+let count = 0;
+const addOne = () => {
+  console.log('addOne');
 };
-
-function getLocation(location) {
-  if (location) {
-    return <p>Location: {location}</p>;
-  }
-}
-
+const minusOne = () => {
+  console.log('minusOne');
+};
+const reset = () => {
+  console.log('reset');
+};
 const templateTwo = (
   <div>
-    <h1>{user.name ? user.name : 'Anonymous'}</h1>
-    {user.age && user.age >= 18 && <p>Age: {user.age}</p>}
-    {getLocation(user.location)}
+    <h1>Count: {count}</h1>
+    <button onClick={addOne}>+1</button>
+    <button onClick={minusOne}>-1</button>
+    <button onClick={reset}>Reset</button>
   </div>
 );
 
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
