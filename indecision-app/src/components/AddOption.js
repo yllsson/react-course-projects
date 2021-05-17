b1c2504efbece63aa55d91f950d5da3c666a77f8
Ylva-Thorsen <ylva.e.thorsen@gmail.com>
2021-05-17 16:26:02 +0100

    style all buttons

--- indecision-app/src/components/AddOption.js
+++ indecision-app/src/components/AddOption.js
@@ -5,7 +5,7 @@ export default class AddOption extends React.Component {
     error: undefined
   };
 
-  handleAddOption = event => {
+  handleAddOption = (event) => {
     event.preventDefault();
 
     /* 
@@ -29,7 +29,7 @@ export default class AddOption extends React.Component {
         {this.state.error && <p>{this.state.error}</p>}
         <form onSubmit={this.handleAddOption}>
           <input type='text' name='option' />
-          <button>Add Option</button>
+          <button className='button'>Add Option</button>
         </form>
       </div>
     );
