a5aaad99f61e72f97297843f0fecc36b074a5696
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-06-09 22:15:35 +0100

    Styling the modal

--- indecision-app/src/components/OptionModal.js
+++ indecision-app/src/components/OptionModal.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import Modal from 'react-modal';
 
-const OptionModal = (props) => (
+const OptionModal = props => (
   <Modal
     isOpen={!!props.selectedOption}
     onRequestClose={props.handleClearSelected}
     contentLabel='Selected Option'
+    closeTimeoutMS={200}
+    className='modal'
     ariaHideApp={false}
   >
-    <h3>Selected Option</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
-    <button onClick={props.handleClearSelected}>Okay</button>
+    <h3 className='modal__title'>Selected Option</h3>
+    {props.selectedOption && (
+      <p className='modal__body'>{props.selectedOption}</p>
+    )}
+    <button className='button' onClick={props.handleClearSelected}>
+      Okay
+    </button>
   </Modal>
 );
 
