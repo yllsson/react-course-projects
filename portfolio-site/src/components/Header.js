d9e5ee65e0a412245e48c53df883f9a0e35cd9e2
Ylva-Thorsen <ylva.e.thorsen@gmail.com>
2021-06-21 12:12:29 +0100

    minor amendment after seeing course solution

--- portfolio-site/src/components/Header.js
+++ portfolio-site/src/components/Header.js
@@ -4,11 +4,15 @@ import { NavLink } from 'react-router-dom';
 const Header = () => (
   <header>
     <h1>Portfolio</h1>
-    <NavLink to='/' exact={true}>
+    <NavLink to='/' activeClassName='is-active' exact={true}>
       Home
     </NavLink>
-    <NavLink to='/portfolio'>Portfolio</NavLink>
-    <NavLink to='/contact'>Contact</NavLink>
+    <NavLink to='/portfolio' activeClassName='is-active' exact={true}>
+      Portfolio
+    </NavLink>
+    <NavLink to='/contact' activeClassName='is-active'>
+      Contact
+    </NavLink>
   </header>
 );
 
