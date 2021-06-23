99e6a92219f37d28c632f0ab6213283e72f9fc14
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-06-23 22:31:06 +0100

    learning about actions and dispatch using counter example

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -1,7 +1,39 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }) => {
-  return state;
+const store = createStore((state = { count: 0 }, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      };
+    case 'RESET':
+      return {
+        count: 0
+      };
+    default:
+      return state;
+  }
+});
+
+console.log(store.getState());
+
+// increment count
+store.dispatch({
+  type: 'INCREMENT'
+});
+
+// decrement count
+store.dispatch({
+  type: 'DECREMENT'
+});
+
+// reset count
+store.dispatch({
+  type: 'RESET'
 });
 
 console.log(store.getState());
