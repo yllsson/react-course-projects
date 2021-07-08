7453b7c1c110177b773f329ae3975ef85d38df51
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-07-08 20:32:59 +0100

    Object destructuring practice

---
+++ expensify-app/src/playground/destructuring.js
@@ -0,0 +1,32 @@
+// Object Destructuring
+
+// const person = {
+//   name: 'Ylva',
+//   age: 31,
+//   location: {
+//     city: 'Edinburgh',
+//     temp: 20
+//   }
+// };
+
+// const { name: firstName = 'Anonymous', age } = person;
+
+// console.log(`${firstName} is ${age}.`);
+
+// const { city, temp: temperature } = person.location;
+
+// if (city && temperature) {
+//   console.log(`It's ${temperature} degrees in ${city}.`);
+// }
+
+const book = {
+  title: 'Ego is the Enemy',
+  author: 'Ryan Holiday',
+  publisher: {
+    name: 'Penguin'
+  }
+};
+
+const { name: publisherName = 'Self-Published' } = book.publisher;
+
+console.log(publisherName);
