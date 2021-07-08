c1de78f3d15ee95363fbc0dd07d0d8e569ff7e10
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-07-08 20:43:20 +0100

    Array destructuring practice

--- expensify-app/src/playground/destructuring.js
+++ expensify-app/src/playground/destructuring.js
@@ -19,14 +19,26 @@
 //   console.log(`It's ${temperature} degrees in ${city}.`);
 // }
 
-const book = {
-  title: 'Ego is the Enemy',
-  author: 'Ryan Holiday',
-  publisher: {
-    name: 'Penguin'
-  }
-};
+// const book = {
+//   title: 'Ego is the Enemy',
+//   author: 'Ryan Holiday',
+//   publisher: {
+//     name: 'Penguin'
+//   }
+// };
 
-const { name: publisherName = 'Self-Published' } = book.publisher;
+// const { name: publisherName = 'Self-Published' } = book.publisher;
 
-console.log(publisherName);
+// console.log(publisherName);
+
+// Array Destructuring
+
+// const address = ['1299 S Juniper Street', 'Edinburgh', 'Scotland', '12147'];
+// const [, city, state = 'New York'] = address;
+// console.log(`You are in ${city} ${state}.`);
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75'];
+
+const [drink, , medium] = item;
+
+console.log(`A medium ${drink} costs ${medium}`);
