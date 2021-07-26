55be680045bbf0c9e16a8ec016983494b56d09a8
Ylva-Thorsen <ylva.e.thorsen@gmail.com>
2021-07-26 14:41:17 +0100

    Breaking out the expensify redux into different folders

--- expensify-app/webpack.config.js
+++ expensify-app/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/playground/redux-expensify.js',
+  entry: './src/app.js',
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
