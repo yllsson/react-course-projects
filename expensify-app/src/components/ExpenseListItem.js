45f217a3a963141f560edeea93984e0e1ecf199d
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-08-04 22:47:24 +0100

    Building up the EditExpensePage

--- expensify-app/src/components/ExpenseListItem.js
+++ expensify-app/src/components/ExpenseListItem.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { removeExpense } from '../actions/expenses';
+import { Link } from 'react-router-dom';
 
-const ExpenseListItem = ({ dispatch, description, amount, createdAt, id }) => {
+const ExpenseListItem = ({ description, amount, createdAt, id }) => {
   return (
     <div>
-      <h3>{description}</h3>
+      <Link to={`/edit/${id}`}>
+        <h3>{description}</h3>
+      </Link>
       <p>
         {amount} - {createdAt}
       </p>
-      <button
-        onClick={() => {
-          dispatch(removeExpense({ id }));
-        }}
-      >
-        Remove
-      </button>
     </div>
   );
 };
 
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
