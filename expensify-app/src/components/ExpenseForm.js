510ca306a029737207ac1cc636140c892db6d4c3
Ylva-Thorsen <ylva.e.thorsen@gmail.com>
2021-08-04 17:57:46 +0100

    Adding date picker to form using react-dates and momentjs

--- expensify-app/src/components/ExpenseForm.js
+++ expensify-app/src/components/ExpenseForm.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import moment from 'moment';
+import 'react-dates/initialize';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+
+const now = moment();
+console.log(now.format('MMM Do, YYYY'));
 
 export default class ExpenseForm extends React.Component {
   state = {
     description: '',
     note: '',
-    amount: ''
+    amount: '',
+    createdAt: moment(),
+    calendarFocused: false
   };
 
   onDescriptionChange = (event) => {
@@ -27,6 +36,14 @@ export default class ExpenseForm extends React.Component {
     }
   };
 
+  onDateChange = (createdAt) => {
+    this.setState(() => ({ createdAt }));
+  };
+
+  onFocusChange = ({ focused }) => {
+    this.setState(() => ({ calendarFocused: focused }));
+  };
+
   render() {
     return (
       <div>
@@ -44,6 +61,15 @@ export default class ExpenseForm extends React.Component {
             placeholder='Amount'
             value={this.state.amount}
             onChange={this.onAmountChange}
+          />
+
+          <SingleDatePicker
+            date={this.state.createdAt}
+            onDateChange={this.onDateChange}
+            focused={this.state.calendarFocused}
+            onFocusChange={this.onFocusChange}
+            numberOfMonths={1}
+            isOutsideRange={() => false}
           />
 
           <textarea
