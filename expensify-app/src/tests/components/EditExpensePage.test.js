da7894753d59fc85981189fa3932ecaa1ec930c8
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-10-11 12:44:29 +0100

    Testing EditExpensePage component

---
+++ expensify-app/src/tests/components/EditExpensePage.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { EditExpensePage } from '../../components/EditExpensePage';
+import expenses from '../fixtures/expenses';
+
+let expense, editExpense, removeExpense, history, wrapper;
+
+beforeEach(() => {
+  expense = expenses[1];
+  editExpense = jest.fn();
+  removeExpense = jest.fn();
+  history = {
+    push: jest.fn()
+  };
+  wrapper = shallow(
+    <EditExpensePage
+      expense={expense}
+      editExpense={editExpense}
+      removeExpense={removeExpense}
+      history={history}
+    />
+  );
+});
+
+test('should render EditExpensePage correctly', () => {
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('should handle editExpense', () => {
+  wrapper.find('ExpenseForm').prop('onSubmit')(expense);
+  expect(history.push).toHaveBeenLastCalledWith('/');
+  expect(editExpense).toHaveBeenLastCalledWith(expense.id, expense);
+});
+
+test('should handle removeExpense', () => {
+  wrapper.find('button').simulate('click');
+  expect(history.push).toHaveBeenLastCalledWith('/');
+  expect(removeExpense).toHaveBeenLastCalledWith({ id: expense.id });
+});
